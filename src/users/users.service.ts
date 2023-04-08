832a7ce331b2639fd3351a412b60e3c1666deb43
chisun joung <csjoung@gmail.com>
2023-04-08 15:28:45 +0900

    5.6

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,6 +22,7 @@ export class UserService {
         return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
       // make error
       return { ok: false, error: "Couldn't create account" };
