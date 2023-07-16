02b05cea74f65aa06620277dcd2c0e16b0d8a8b6
chisun joung <csjoung@gmail.com>
2023-07-16 16:55:27 +0900

    11.4

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,24 +1,22 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import {
-  CreateRestaurantDto,
-  CreateRestaurantInput,
-} from './dtos/create-restaurant.dto';
-import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantsService } from './restaurants.service';
-import { CreateAccountOutput } from 'src/users/dtos/create-account.dto';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { User } from 'src/users/entities/user.entity';
+import {
+  CreateRestaurantInput,
+  CreateRestaurantOutput,
+} from './dtos/create-restaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantsResolver {
   constructor(private readonly restaurantsService: RestaurantsService) {}
 
-  @Mutation((returns) => CreateAccountOutput)
+  @Mutation((returns) => CreateRestaurantOutput)
   async createRestaurant(
     @AuthUser() authUser: User,
     @Args('input') createRestaurantInput: CreateRestaurantInput,
-  ): Promise<CreateAccountOutput> {
+  ): Promise<CreateRestaurantOutput> {
     return this.restaurantsService.createRestaurant(
       authUser,
       createRestaurantInput,
